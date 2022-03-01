d1a6e764bb7fca51b2a8e286c78a591b8a921d22
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-03-01 23:04:28 +0600

    product details added

--- js/phoneDetails.js
+++ js/phoneDetails.js
@@ -19,9 +19,73 @@ const showPhoneDetails = (phoneDetails) => {
   
   const {name, releaseDate, brand} = phoneDetails;
   const mainFeatures = phoneDetails.mainFeatures;
-  console.log(mainFeatures);
   const { chipSet, displaySize, memory, storage} = mainFeatures;
   const sectionPhoneDetails = document.querySelector("#section-phone-details");
+  const sensors = mainFeatures.sensors.join(", ");
+  let others, WLAN, Bluetooth, GPS, NFC, Radio, USB;
+  if(phoneDetails.hasOwnProperty('others')){
+    others = phoneDetails.others;
+
+    WLAN = others.WLAN;
+    Bluetooth = others.Bluetooth;
+    GPS = others.GPS;
+    NFC = others.NFC;
+    Radio = others.Radio;
+    USB = others.USB;
+  }
+  else{
+    others = null;
+  }
+
+  const othersHtml = `<h5 class="text-decoration-underline fw-bold mb-4">Others:</h5>
+  <div class="row">
+    <div class="col-4">
+      <p class="fw-bold">WLAN:</p> 
+    </div>
+    <div class="col-8">
+      <p>${WLAN}</p> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <p class="fw-bold">Bluetooth:</p> 
+    </div>
+    <div class="col-8">
+      <p>${Bluetooth}</p> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <p class="fw-bold">GPS:</p> 
+    </div>
+    <div class="col-8">
+      <p>${GPS}</p> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <p class="fw-bold">NFC:</p> 
+    </div>
+    <div class="col-8">
+      <p>${NFC}</p> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <p class="fw-bold">Radio:</p> 
+    </div>
+    <div class="col-8">
+      <p>${Radio}</p> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <p class="fw-bold">USB:</p> 
+    </div>
+    <div class="col-8">
+      <p>${USB}</p> 
+    </div>
+  </div>`;
 
   const phoneDetailsHtml = `<div class="container mb-5 bg-white p-3 rounded shadow-lg container-phone-details">
   <h1 class="text-center mb-5">Phone Details</h1>
@@ -94,58 +158,10 @@ const showPhoneDetails = (phoneDetails) => {
               <p class="fw-bold">Sensors:</p> 
             </div>
             <div class="col-8">
-              <p>Face ID, accelerometer, gyro, proximity, compass, barometer</p> 
+              <p>${sensors}</p> 
             </div>
           </div>
-          <h5 class="text-decoration-underline fw-bold mb-4">Others:</h5>
-          <div class="row">
-            <div class="col-4">
-              <p class="fw-bold">WLAN:</p> 
-            </div>
-            <div class="col-8">
-              <p>Wi-Fi 802.11 a/b/g/n/ac/6, dual-band, hotspot</p> 
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <p class="fw-bold">Bluetooth:</p> 
-            </div>
-            <div class="col-8">
-              <p>5.0, A2DP, LE</p> 
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <p class="fw-bold">GPS:</p> 
-            </div>
-            <div class="col-8">
-              <p>Yes, with A-GPS, GLONASS, GALILEO, BDS, QZSS</p> 
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <p class="fw-bold">NFC:</p> 
-            </div>
-            <div class="col-8">
-              <p>Yes</p> 
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <p class="fw-bold">Radio:</p> 
-            </div>
-            <div class="col-8">
-              <p>No</p> 
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <p class="fw-bold">USB:</p> 
-            </div>
-            <div class="col-8">
-              <p>Lightning, USB 2.0</p> 
-            </div>
-          </div>
+          ${others? othersHtml : ""}
         </div>
       </div>
     </div>
