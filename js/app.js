2a7f62450580e3660849a538acceec78c9c16982
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-03-01 13:55:28 +0600

    phone list sowing on search is done

--- js/app.js
+++ js/app.js
@@ -10,12 +10,36 @@ btnSearch.addEventListener("click", async (e)=>{
   try {
     const result = await fetchPhoneList(inputSearch);
     const phoneList = result.data;
-    phoneList.forEach((item)=>{
-      console.log(item);
-    })
+    showPhoneList(phoneList);
   } catch (error) {
     console.log(error);
   }
 })
+
+const showPhoneList = (phoneList) => {
+  const phoneListContainer = document.querySelector(".phone-list-container");
+
+  const phoneListHtml = phoneList
+    .map((phone) => {
+      const {
+        phone_name : phoneName,
+        brand,
+        image,
+      } = phone;
+      return `<div class="col-12 col-md-6 col-lg-4">
+      <div class="card p-2">
+        <img src="${image}" style="height:25rem" class="card-img-top" alt="${phoneName}">
+        <div class="mt-2 body-details">
+          <h5 class="card-title">Name: ${phoneName}</h5>
+          <p class="card-text mb-5">Brand: ${brand}</p>
+          <a href="#" class="btn btn-primary btn-details">Details</a>
+        </div>
+      </div>
+    </div>`;
+    })
+    .join("");
+
+    phoneListContainer.innerHTML = phoneListHtml;
+}
 
 
